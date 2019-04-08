5326d514046ec27f7cf7dcedcd823a8ba7a7433d
Paul Galinsky <paul@designimage.eu>
2019-04-08 14:50:08 +0100

    Removed most CSS and set some base styles

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 		<meta charset="UTF-8">
 		<title>Design Image</title>
 		<?php wp_head(); ?>
+		<link href="https://fonts.googleapis.com/css?family=Lemon|PT+Sans:400,700" rel="stylesheet">
 	</head>
 	<body>
 
