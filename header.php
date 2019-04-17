c74bdd38c1575439e4e1ab5f3d650aef3c4c1cb9
Paul Galinsky <paul@designimage.eu>
2019-04-17 12:04:48 +0100

    increased base font sizes, added containers

--- header.php
+++ header.php
@@ -8,11 +8,13 @@
 	</head>
 	<body <?php body_class(); ?>>
 		<header>
-			<div class="logo">
-				<h1><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" width="160" height="160" alt="Design Image Logo" /></h1>
-			</div><!-- .logo -->
-			<div class="intro">
-				<p>Hello, Paul here! I’m now half of the web and print duo <a href="https://www.lumpylemon.co.uk">Lumpy Lemon</a>, so this website is just a cheeky little showcase of some of my creative work. Enjoy!</p>
-				<p>Oh, and if we haven’t spoken in a while, I’ve married, changed my surname to Galinsky and moved to Edinburgh. It’s all change!</p>
-			</div> <!-- .intro -->
+			<div class="container">
+				<div class="logo">
+					<h1><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" width="160" height="160" alt="Design Image Logo" /></h1>
+				</div><!-- .logo -->
+				<div class="intro">
+					<p>Hello, Paul here! I’m now half of the web and print duo <a href="https://www.lumpylemon.co.uk">Lumpy Lemon</a>, so this website is just a cheeky little showcase of some of my creative work. Enjoy!</p>
+					<p>Oh, and if we haven’t spoken in a while, I’ve married, changed my surname to Galinsky and moved to Edinburgh. It’s all change!</p>
+				</div> <!-- .intro -->
+			</div><!-- .container -->
 		</header>
--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php get_header(); ?>
 <main>
-
-	<div class="content">
-		<?php
-			if( have_posts() ):
-				while ( have_posts() ): the_post();?>
-					<div <?php post_class(); ?>>
-						<div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
-						<h2><?php the_title(); ?></h2>
-						<p><?php the_content(); ?></p>
-						<?php the_tags( '<ul class="tags"><li>', '</li><li>', '</li></ul>' ); ?>
-						<a href="<?php the_permalink(); ?>" class="post-link"></a>
-					</div>
-				<?php endwhile;
-			endif;
-		?>
-	</div> <!-- .content -->
+	<div class="container">
+		<div class="content">
+			<?php
+				if( have_posts() ):
+					while ( have_posts() ): the_post();?>
+						<div <?php post_class(); ?>>
+							<div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
+							<h2><?php the_title(); ?></h2>
+							<p><?php the_content(); ?></p>
+							<?php the_tags( '<ul class="tags"><li>', '</li><li>', '</li></ul>' ); ?>
+							<a href="<?php the_permalink(); ?>" class="post-link"></a>
+						</div>
+					<?php endwhile;
+				endif;
+			?>
+		</div> <!-- .content -->
+	</div> <!-- .container -->
 </main>
 <?php get_footer(); ?>
