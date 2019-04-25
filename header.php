7825e2af94482da007c2321313387209f0d7cdce
Paul Galinsky <paul@designimage.eu>
2019-04-25 16:48:23 +0100

    Updated header logo and main font

--- header.php
+++ header.php
@@ -4,16 +4,20 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Design Image</title>
 		<?php wp_head(); ?>
-		<link href="https://fonts.googleapis.com/css?family=Lemon|PT+Sans:400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700|Lemon" rel="stylesheet">
 	</head>
 	<body <?php body_class(); ?> id="site">
 		<header>
 			<div class="container">
 				<div class="logo">
-					<h1><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" width="160" height="160" alt="Design Image Logo" /></h1>
+					<h1>
+						<a href="<?php echo site_url(); ?>" rel="home" title="Link to Lumpy Lemon homepage" aria-label="Link to Lumpy Lemon homepage">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" width="160" height="160" alt="Design Image Logo" />
+						</a>
+					</h1>
 				</div><!-- .logo -->
 				<div class="intro">
-					<p>Hello, Paul here! I’m now half of the web and print duo <a href="https://www.lumpylemon.co.uk">Lumpy Lemon</a>, so this website is just a cheeky little showcase of some of my creative work. Enjoy!</p>
+					<p>Hello, Paul here! I’ve retired designimage.eu and I'm now half of the magnificent web and print duo <a href="https://www.lumpylemon.co.uk">Lumpy Lemon</a>.</p>
 					<p>Oh, and if we haven’t spoken in a while, I’ve married, changed my surname to Galinsky and moved to Edinburgh. It’s all change!</p>
 				</div> <!-- .intro -->
 			</div><!-- .container -->
