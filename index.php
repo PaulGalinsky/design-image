d09780ab8d7601c4dbf187dcbf89f69458961a99
Paul Galinsky <paul@designimage.eu>
2019-04-29 20:49:49 +0100

    Very basic homepage complete

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 							<div class="post-primary-content">
 								<!-- next two lines pull in the featured image as a background image as at https://bit.ly/2ZMtvt4 -->
 								<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
-								<div class="thumbnail-img" style="background-image: url('<?php echo $backgroundImg[0]; ?>')"></div>
-								<a href="<?php the_permalink(); ?>" class="post-link"></a>
+								<a href="<?php the_permalink(); ?>"><div class="thumbnail-img" style="background-image: url('<?php echo $backgroundImg[0]; ?>')"></div></a>
 							</div>
 							<div class="post-secondary-content">
-								<h2><?php the_title(); ?></h2>
+								<a href="<?php the_permalink(); ?>" class="read-more"><h2><?php the_title(); ?></h2></a>
 								<p><?php echo get_first_paragraph(); ?></p>
+								<a href="<?php the_permalink(); ?>" class="read-more"> >> Read more >> </a>
 								<!-- <p ><?php the_content(); ?></p> -->
 								<?php the_tags( '<ul class="tags"><li>', '</li><li>', '</li></ul>' ); ?>
 							</div>
