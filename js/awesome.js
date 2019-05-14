a7e89202f26ff4c696add8669f8c45621da73cac
Paul Galinsky <paul@designimage.eu>
2019-05-14 21:53:37 +0100

    added jquery and initial cookie message

---
+++ js/awesome.js
@@ -0,0 +1,4 @@
+// cookie message
+jQuery( document ).ready( function( $ ) {
+	$('#site').prepend('<div class="cookie-notice"><p>Yeah this site uses cookies. <a href="http://localhost:5757/privacy-policy/">Learn more</a>, navigate away, or just keep on reading like everyone else.</p></div>');
+});
