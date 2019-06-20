bb5d1f34b21c421db2d50afc24dea43ba28418a1
Paul Galinsky <paul@designimage.eu>
2019-06-20 12:49:14 +0100

    Curly braces on function.php user access

--- functions.php
+++ functions.php
@@ -13,15 +13,15 @@ remove_action('welcome_panel', 'wp_welcome_panel');
 
 function remove_menus() {
 
-	if ( current_user_can( 'import' ) )
-		return;
-
+	if ( current_user_can( 'import' ) ) {
+	}	return; {
 	remove_menu_page( 'edit-comments.php' );          //Comments
 	remove_menu_page( 'themes.php' );                 //Appearance
-	// remove_menu_page( 'plugins.php' );                //Plugins
+	remove_menu_page( 'plugins.php' );                //Plugins
 	remove_menu_page( 'users.php' );                  //Users
-	// remove_menu_page( 'tools.php' );                  //Tools
+	remove_menu_page( 'tools.php' );                  //Tools
 	remove_menu_page( 'options-general.php' );        //Settings
+	}
 }
 add_action( 'admin_menu', 'remove_menus' );
 
