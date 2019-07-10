e3329ac3f4f4ae814dae319ea16bdf3a7682556e
Simon Blackbourn <simon@lumpylemon.co.uk>
2019-07-10 12:07:04 +0100

    Remove title tage from header

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Design Image</title>
 		<?php wp_head(); ?>
 		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700|Lemon" rel="stylesheet">
 	</head>
